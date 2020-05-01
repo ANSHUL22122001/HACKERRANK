3e2de3c4ca29bbf07fd973c1f3193acd1842a3be
ANSHUL22122001 <62570770+ANSHUL22122001@users.noreply.github.com>
2020-05-02 04:22:43 +0530

    folder
    folder change

---
+++ Algorithum/Bird Migration.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner object= new Scanner(System.in);
+        int times=0,ans=0,sum;
+        int limit = object.nextInt();
+        int arr[] = new int[limit];
+        for(int i=0; i<limit; i++) arr[i] = object.nextInt();
+        Arrays.sort(arr);
+        for(int i = 0; i<limit; i++){
+            sum=0;
+            for(int j=i+1;j<limit; j++){
+              if(arr[i] == arr[j]){sum++;}
+              else {break;}
+            }
+            if(sum > times){
+                //System.out.println(ans);
+                ans=arr[i];
+                times=sum;
+            }
+        } 
+        System.out.println(ans);
+    }
+}
